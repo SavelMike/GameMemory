a84eda1a37e4376e429f5f100915c23f61b006b0
Saveliev Mikhail <Savelievmv@gmail.com>
2017-01-21 19:50:27 +0100

    Methods

--- src/MemoryGame.java
+++ src/MemoryGame.java
@@ -3,28 +3,48 @@
  */
 public class MemoryGame {
     public static void main(String[] args) {
+
         /**
-         *  1) 1) Create array with pair digits, fill second array opened with zeroes;
-         public void fillArray(int[] Array, int[] opened)
-         *  2) Display array;  user's index for array
-         *  public void displayArray(int[] Array, int ind1, int ind2, int[] opened);
-         *  3) public boolean isGameOver(int[] opened);
-         *
-         */
+         *  1 create array and fill it (call method fillArray());
+         **/
         main()
         {
-        /* 1 create array and fill it (call method fillArray()), create second array with zeroes  */
-            fillArray();
-
-        /* 2 game loop */
-            /* public boolean isGameOver(int[] opened) */
+            /* 2 game loop */
             while (isGameOver() == false) {
                 /* ask user 2 index */
                 System.out.println();
-
                 /* print an array */
                 displayArray();
-
             }
-    }
+        }
+
+        /**
+         *  1) Create array with pair digits, fill second array opened with zeroes;
+         *  2) 2 arrays are parameters
+         **/
+        public static void fillArray(int[] Array, int[] opened)
+            {
+            }
+
+    /**
+     * 2) Display array;  user's index for array
+     * @param Array
+     * @param ind1 index of opened element
+     * @param ind2 index of opened element
+     * @param opened
+     */
+        public static void displayArray((int[] Array, int ind1, int ind2, int[] opened))
+            {
+            }
+
+    /**
+     * If array opened doesn't contain 0;
+     * @param opened
+     */
+    public static void isGameOver(int[] opened)
+        {
+        }
+}
+
+
 }
